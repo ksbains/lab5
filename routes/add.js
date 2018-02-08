3186759c387a4d29da03c89cbe91a1c763b0af3d
ksbains <k1bains@ucsd.edu>
2018-02-08 10:15:49 -0800

    added the render line after the update to dynapmicall add new content

--- routes/add.js
+++ routes/add.js
@@ -1,14 +1,15 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	let friend = {
+exports.addFriend = function(req, res){
+ 	
+	let friend = {	
 		name: req.query.name,
 		description: req.query.description,
 		imageURL: 'http://lorempixel.com/400/400/people'
-		//imageURL: "https://assets.vogue.com/photos/589151c258aa89a00d542b38/master/pass/00-5-things-emma-stone.jpg"
 	}
 	console.log("BREAK");
 	console.log("the friend to push is ", friend);
 	console.log("BREAK");
 	data.friends.push(friend);
- }
+	res.render("index", data);
+}
