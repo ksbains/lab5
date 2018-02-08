536c7e2aab35f9e2ca60c4e54fbf407c069b1f67
ksbains <k1bains@ucsd.edu>
2018-02-08 10:22:37 -0800

    changed the http to an https

--- routes/add.js
+++ routes/add.js
@@ -5,8 +5,9 @@ exports.addFriend = function(req, res){
 	let friend = {	
 		name: req.query.name,
 		description: req.query.description,
-		imageURL: 'http://lorempixel.com/400/400/people'
+		imageURL: 'https://lorempixel.com/400/400/people'
 	}
+
 	console.log("BREAK");
 	console.log("the friend to push is ", friend);
 	console.log("BREAK");
